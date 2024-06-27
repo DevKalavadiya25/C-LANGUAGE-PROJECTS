f21629879299daec7ad5a543df8a094897da2ab8
DevKalavadiya25 <167860339+DevKalavadiya25@users.noreply.github.com>
2024-06-27 10:56:15 +0530

    Update project-4 Q-7.cpp

--- project-1 (Cricket)/project-4 Q-7.cpp
+++ project-1 (Cricket)/project-4 Q-7.cpp
@@ -4,10 +4,10 @@ for(int row=1 ; row<=5 ; row++){
 
 for(int col=1 ; col<=5 ; col++){
 
-if(col==1 || row==1 || col==1 || row==3){
+if(col==1 || row==1 || col==1 || row==3 || col==5&&row<=3){
 printf("* ");
 }else{
-   printf(" "); 
+   printf("  "); 
 }
 
 }
